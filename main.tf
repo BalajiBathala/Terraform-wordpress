213a953a33c988c93d1d0ee096c7d7babc69cbbc
BalajiBathala <vijayabalajib2@gmail.com>
2025-01-06 13:04:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "wordpress" {
   user_data = file("userdata.sh")
 
   tags = {
-    Name = "WordPress-Instance"
+    Name = "WordPress"
   }
 
   security_groups = [aws_security_group.wordpress_sg.name]
